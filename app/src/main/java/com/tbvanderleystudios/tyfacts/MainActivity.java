8145c237e43dea56599583dbdb905ce00ceb9b49
TBVanderley4545 <TBVanderley@gmail.com>
2016-01-11 23:12:43 -0500

    Added FactBook class for facts along with its method to generate a random fact. MainActivity is responsible for updating the layout with a new random fact.

--- app/src/main/java/com/tbvanderleystudios/tyfacts/MainActivity.java
+++ app/src/main/java/com/tbvanderleystudios/tyfacts/MainActivity.java
@@ -2,13 +2,19 @@ package com.tbvanderleystudios.tyfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // Declare our View variables as fields (which are variables that are class level)
     private TextView mFunFactTextView;
     private Button mNewFactButton;
+    private RelativeLayout mRelativeLayout;
+
+    // Create a FactBook object as a field
+    private FactBook mFactBook = new FactBook();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
         // Assign the View from the layout file to the corresponding variables
         mFunFactTextView = (TextView) findViewById(R.id.funFactTextView);
         mNewFactButton = (Button) findViewById(R.id.newFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The String fact gets a fact from the String[] in the FactBook class.
+                String fact = mFactBook.getFact();
+
+                // Update the screen with our dynamic fact
+                mFunFactTextView.setText(fact);
+            }
+        };
+        mNewFactButton.setOnClickListener(listener);
     }
 }
